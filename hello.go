f90e2ca6a0ec016eed4ef91b26ff91a436af29fe
gisagisa <68657742+gisagisa@users.noreply.github.com>
2020-07-23 00:03:21 +0700

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var namakakak string = "Merosa Pesona"
+
+	namaadik := "Anosep Asorem"
+
+	fmt.Println("Nama Kakak : ", namakakak)
+	fmt.Println("Nama Adik : ", namaadik)
+}
